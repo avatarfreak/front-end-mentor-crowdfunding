8e62fcf39aed795158953d4d054cf8be57532b2d
avatarfreak <avatarfreak83@gmail.com>
2021-06-14 13:14:35 +0530

    rename form to FormContent , extra elements added

--- app/components/Modules/Main/ModalContent/Form/index.tsx
+++ app/components/Modules/Main/ModalContent/Form/index.tsx
@@ -1,37 +1,53 @@
 import { useState } from "react";
-import { Title, Body, Input, Button, Currency, Error } from "@/Elements/Main/ModalContent/Form";
+import {
+  Title,
+  Body,
+  Input,
+  Button,
+  Currency,
+  InputContainer,
+  Wrapper,
+  Error,
+} from "@/Elements/Main/ModalContent/Form";
 import { ModalContent } from "@/Modules/Main/ModalContent/ThankModal";
 import { Modal } from "@/Modules/Modal/Modal";
 
-export const Form = ({ amount }) => {
+export const FormContent = ({ amount }) => {
   const [value, setValue] = useState<number>(amount);
   const [isActive, setIsActive] = useState<boolean>(false);
   const [error, setError] = useState<string>("");
 
   const validatePledge = (e: React.SyntheticEvent) => {
     e.preventDefault();
+
     if (value < amount) {
       setError(`Pledge should be equal or more than ${amount}`);
       return;
     }
+
     setIsActive(!isActive);
     setError("");
+    setValue(amount);
   };
 
   return (
     <>
       <form onSubmit={validatePledge} noValidate>
-        <Title> Enter your pledge </Title>
-        <Body>
-          <Currency>$</Currency>
-          <Input
-            min={amount}
-            placeholder={amount}
-            value={value}
-            onChange={(e) => setValue(parseFloat(e.target.value))}
-          />
-          <Button>Continue</Button>
-        </Body>
+        <Wrapper>
+          <Title> Enter your pledge </Title>
+          <Body>
+            <InputContainer>
+              <Currency>$</Currency>
+              <Input
+                min={amount}
+                placeholder={amount}
+                value={value}
+                onChange={(e) => setValue(parseFloat(e.target.value))}
+              />
+            </InputContainer>
+            <Button>Continue</Button>
+          </Body>
+        </Wrapper>
         <Error>{error}</Error>
       </form>
       <Modal showModal={isActive}>
