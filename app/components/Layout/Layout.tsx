d6fd23ef91d773cba98d758fad2b493c89d05228
avatarfreak <avatarfreak83@gmail.com>
2021-06-14 13:12:38 +0530

    Themeprovider added

--- app/components/Layout/Layout.tsx
+++ app/components/Layout/Layout.tsx
@@ -1,16 +1,18 @@
 import Head from "next/head";
-import GlobalStyle from "./GlobalStyle";
+import { ThemeProvider } from "styled-components";
+import Theme from "@/Theme/theme";
+import GlobalStyle from "@/Layout/GlobalStyle";
 
 const Layout: React.FC<React.ReactNode> = ({ children }) => {
   return (
-    <>
+    <ThemeProvider theme={Theme}>
       <GlobalStyle />
       <Head>
         <meta name="description" content="Crowdfunding product page" />
         <title>Frontend Mentor | Crowdfunding product page</title>
       </Head>
       {children}
-    </>
+    </ThemeProvider>
   );
 };
 
