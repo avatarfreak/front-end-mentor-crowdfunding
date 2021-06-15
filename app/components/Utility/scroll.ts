93cee9de983ab34d6654ed9836325fba34ee897e
avatarfreak <avatarfreak83@gmail.com>
2021-06-15 16:42:21 +0530

    window scroll added

--- app/components/Utility/scroll.ts
+++ app/components/Utility/scroll.ts
@@ -1,1 +1,9 @@
-export const bodyScroll = (option = "auto") => document.querySelector('body').style.overflow = option;
+export const bodyScroll = (option = "auto") => {
+  window.scrollTo({
+    top: 0,
+    left: 0,
+    behavior: "smooth",
+  });
+  document.querySelector("body").style.overflow = option;
+};
+
