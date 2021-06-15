7a5aea8d3eccde59404d6d849dfe21739f84634b
avatarfreak <avatarfreak83@gmail.com>
2021-06-15 20:52:42 +0530

    prefix url added

--- app/components/Modules/Navbar/Nav/MenuTrigger/index.tsx
+++ app/components/Modules/Navbar/Nav/MenuTrigger/index.tsx
@@ -1,4 +1,5 @@
 import { ImageWrapper } from "@/Elements/Header/Navbar/Nav/Menu";
+import { prefix } from "@/Utility/prefix";
 import Image from "next/image";
 import { IProps } from "./type";
 
@@ -7,9 +8,19 @@ export const MenuTrigger: React.FC<IProps> = ({ active, setActive }) => {
     <>
       <ImageWrapper onClick={() => setActive((prev) => !prev)}>
         {!active ? (
-          <Image width="15" height="15" src="/images/icon-hamburger.svg" alt="hamburger menu" />
+          <Image
+            width="15"
+            height="15"
+            src={`${prefix}/images/icon-hamburger.svg`}
+            alt="hamburger menu"
+          />
         ) : (
-          <Image width="15" height="15" src="/images/icon-close-menu.svg" alt="close menu icon" />
+          <Image
+            width="15"
+            height="15"
+            src={`${prefix}/images/icon-close-menu.svg`}
+            alt="close menu icon"
+          />
         )}
       </ImageWrapper>
     </>
