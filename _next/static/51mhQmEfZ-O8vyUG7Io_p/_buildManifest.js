ba72191794eabb579635a707ec5adc31333d6769
avatarfreak <avatarfreak83@gmail.com>
2021-06-16 05:31:06 +0000

    Deploying to gh-pages from @ avatarfreak/front-end-mentor-crowdfunding@9ff9a74cc4b5b959b4e2cd9cae03dfb4d386a631 🚀

--- _next/static/51mhQmEfZ-O8vyUG7Io_p/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/61-6b59e8bd85c3c0022c0f.js","static/chunks/pages/index-7cfe4beeaf1c0aace1cf.js"],"/_error":["static/chunks/pages/_error-d36babdecdd567a38448.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ _next/static/_JooIVyDUgrYNEvGXf72M/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/61-6b59e8bd85c3c0022c0f.js","static/chunks/pages/index-a88860e97d08a7abf9fb.js"],"/_error":["static/chunks/pages/_error-d36babdecdd567a38448.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
