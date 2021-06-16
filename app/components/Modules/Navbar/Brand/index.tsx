5ee97412fe1b86d19e49b8109a5e7ceb9dd9bb01
avatarfreak <avatarfreak83@gmail.com>
2021-06-16 10:45:25 +0530

    adding trailingslash to configuration

--- app/components/Modules/Navbar/Brand/index.tsx
+++ app/components/Modules/Navbar/Brand/index.tsx
@@ -1,19 +1,13 @@
 import Link from "next/link";
 import Image from "next/image";
 import { Brand } from "@/Elements/Header/Navbar/Brand";
-import { prefix } from "@/Utility/prefix";
 
 const Logo: React.FC<React.ReactNode> = () => {
   return (
     <Brand>
       <Link href="/">
         <a>
-          <Image
-            width="128"
-            height="20"
-            src={prefix + "/images/logo.svg"}
-            alt="crowdfunding Logo"
-          />
+          <Image width="128" height="20" src="/images/logo.svg" alt="crowdfunding Logo" />
         </a>
       </Link>
     </Brand>
