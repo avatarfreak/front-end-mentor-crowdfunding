421b8e97d5d64df95e0d379ee8882757c98c2b26
avatarfreak <avatarfreak83@gmail.com>
2021-06-16 15:51:29 +0530

    added setSjowModal props

--- app/components/Modules/Main/ModalContent/ThankModal/index.tsx
+++ app/components/Modules/Main/ModalContent/ThankModal/index.tsx
@@ -1,12 +1,14 @@
 import { SetStateAction } from "react";
 import Image from "next/image";
 import { Card, Success, Title, Text, Button } from "@/Elements/Main/ModalContent/ThankModal";
+import { bodyScroll } from "@/Utility/scroll";
 
 interface IProps {
   setIsActive: React.Dispatch<SetStateAction<boolean>>;
+  setShowModal: React.Dispatch<SetStateAction<boolean>>;
 }
 
-export const ModalContent = ({ setIsActive }) => {
+export const ModalContent = ({ setIsActive, setShowModal }) => {
   return (
     <Card>
       <Success>
@@ -17,7 +19,15 @@ export const ModalContent = ({ setIsActive }) => {
         Your pledge brings us one step closer to sharing Mastercraft Bamboo Monitor Riser worldwide.
         You will get an email once our campaign is completed.
       </Text>
-      <Button onClick={() => setIsActive((prev) => !prev)}>Got it!</Button>
+      <Button
+        onClick={() => {
+          setIsActive((prev: boolean) => !prev);
+          setShowModal(false);
+          bodyScroll("auto");
+        }}
+      >
+        Got it!
+      </Button>
     </Card>
   );
 };
